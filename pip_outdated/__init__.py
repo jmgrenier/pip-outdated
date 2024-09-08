757f57e9d68e6c4989477326dec7bf7e0d9edb0b
Jean-Michel Grenier <jmgrenier@leslibraires.ca>
2024-09-08 18:21:09 -0400

    Fixed missing sys import; formatting

--- pip_outdated/__init__.py
+++ pip_outdated/__init__.py
@@ -1,46 +1,52 @@
 import argparse
 import asyncio
+import sys
 
 __version__ = "0.7.0"
 
 if sys.platform == 'win32':
-	asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-def parse_args():
+
+def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         prog="pip-outdated",
-        description="Find outdated dependencies in your requirements.txt or "
-                    "setup.cfg file.",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Find outdated dependencies in your requirements.txt, setup.cfg or pyproject.toml file.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print verbose information.")
     parser.add_argument(
-        "-v", "--verbose", action="store_true", help="Print verbose information.")
+        "-q", "--quiet", action="store_true", help="Don't return exit code 1 if not everything is up to date."
+    )
     parser.add_argument(
-        "-q", "--quiet", action="store_true",
-        help="Don't return exit code 1 if not everything is up to date.")
-    parser.add_argument(
-        "file", nargs="*", default=["requirements.txt", "setup.cfg"], metavar="<file>",
-        help="Read dependencies from requirements files. This option accepts "
-             "glob pattern.")
+        "file",
+        nargs="*",
+        default=["requirements.txt", "setup.cfg", "pyproject.toml"],
+        metavar="<file>",
+        help="Read dependencies from requirements files. This option accepts glob pattern.",
+    )
     return parser.parse_args()
-    
-def main():
+
+
+def main() -> None:
     # FIXME: we can't use asyncio.run since it closes the event loop
     # https://github.com/aio-libs/aiohttp/issues/1925
     # asyncio.run(_main())
     asyncio.get_event_loop().run_until_complete(_main())
 
-async def _main():
-    # pylint: disable=import-outside-toplevel
+
+async def _main() -> None:
     args = parse_args()
 
     from .verbose import set_verbose
+
     set_verbose(args.verbose)
-    
-    from .find_require import find_require
+
     from .check_outdated import check_outdated
+    from .find_require import find_require
     from .print_outdated import print_outdated
     from .session import get_session
-    
+
     requires = find_require(args.file)
     async with get_session() as session:
         outdated_results = [asyncio.create_task(check_outdated(r, session)) for r in requires]
