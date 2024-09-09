704efe6a8f5d878f9d733ad1f6b1d0e7f617b8f9
Jean-Michel Grenier <jmgrenier@leslibraires.ca>
2024-09-08 21:52:20 -0400

    Fixed default DNS resolver

--- pip_outdated/session.py
+++ pip_outdated/session.py
@@ -3,5 +3,5 @@ import aiohttp
 
 def get_session() -> aiohttp.ClientSession:
     headers = {"User-Agent": "pip-outdated"}
-    connector = aiohttp.TCPConnector(limit_per_host=5, resolver=aiohttp.DefaultResolver())
+    connector = aiohttp.TCPConnector(limit_per_host=5, resolver=aiohttp.ThreadedResolver())
     return aiohttp.ClientSession(headers=headers, connector=connector)
