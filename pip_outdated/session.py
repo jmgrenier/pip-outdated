b4063c25d341f766e50caa7eb4d3922ed08959ef
Jean-Michel Grenier <jmgrenier@leslibraires.ca>
2024-09-08 21:47:11 -0400

    Added default DNS resolver for faster resolution

--- pip_outdated/session.py
+++ pip_outdated/session.py
@@ -3,5 +3,5 @@ import aiohttp
 
 def get_session() -> aiohttp.ClientSession:
     headers = {"User-Agent": "pip-outdated"}
-    connector = aiohttp.TCPConnector(limit=5)
+    connector = aiohttp.TCPConnector(limit_per_host=5, resolver=aiohttp.DefaultResolver())
     return aiohttp.ClientSession(headers=headers, connector=connector)
